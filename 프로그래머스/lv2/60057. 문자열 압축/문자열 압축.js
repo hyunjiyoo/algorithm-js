44f1dd555f97035f7bb813470df0b1477d72561d
hjyoo.dev <solteee23@gmail.com>
2023-01-13 12:05:22 +0900

    [level 2] Title: 문자열 압축, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,28 @@
+function solution(s) {
+    let answer = Number.MAX_SAFE_INTEGER;
+    
+    if (s.length === 1) return 1;
+    
+    for (let cut = 1; cut <= s.length / 2; ++cut) {
+        let compressedStr = '';
+        
+        let count = 1;
+        let cuttedStr = s.slice(0, cut);
+        for (let j = cut; j < s.length; j += cut) {
+            const nextStr = s.slice(j, j + cut);      
+            
+            if (cuttedStr === nextStr) {
+                count++;
+            } else {
+                compressedStr += (count > 1 ? count : '') + cuttedStr;    
+                cuttedStr = nextStr;
+                count = 1;
+            }  
+        }
+        
+        compressedStr += (count > 1 ? count : '') + cuttedStr;    
+        answer = Math.min(answer, compressedStr.length);
+    }
+    
+    return answer;
+}
