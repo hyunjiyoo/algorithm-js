d9dc4a464ee3437871780b00521aa88bfee11fc1
hjyoo.dev <solteee23@gmail.com>
2023-02-06 15:19:02 +0900

    [level 2] Title: [1차] 캐시, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -6,21 +6,19 @@ function solution(cacheSize, cities) {
     const cityList = cities.map(city => city.toLowerCase());    
     const queue = [];
     let answer = 0;
-
     
     for (const city of cityList) {
         const idx = queue.indexOf(city);
         
         if (idx !== -1) {
-            answer += 1;
-            queue.push(queue[idx]);
             queue.splice(idx, 1);
-            continue;
-        } 
+            answer += 1;
+        } else {
+            (queue.length === cacheSize) && queue.shift();    
+            answer += 5;
+        }
         
-        (queue.length === cacheSize) && queue.shift();    
         queue.push(city);
-        answer += 5;
     }
     
     return answer;
