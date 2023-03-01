d9668270563e06ab6575441f2bb5590fd4ed203d
hjyoo.dev <solteee23@gmail.com>
2023-03-01 23:36:34 +0900

    [level 2] Title: 가장 큰 수, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,10 @@
 function solution(numbers) {
-    const map = new Map();
     
+    if (numbers.every(num => num === 0)) {
+        return "0";
+    }
+    
+    const map = new Map();
     const descend = (a, b) => {
         if (a[1] < b[1]) return 1;
         if (a[1] > b[1]) return -1;
@@ -10,15 +14,8 @@ function solution(numbers) {
     numbers.forEach((num, i) => {
         const numStr = num.toString();
         const value = numStr.repeat(4).slice(0, 4);
-
-        // console.log(value);
-        map.set(`${num}_${i}`, value);
+        map.set(`${num}-${i}`, value);
     });
     
-    if (numbers.every(num => num === 0)) {
-        return "0";
-    }
-    
-    // console.log([...map].sort(descend).map(m => m[0].split('_')[0]).join(''));
-    return [...map].sort(descend).map(m => m[0].split('_')[0]).join('');
+    return [...map].sort(descend).map(m => m[0].split('-')[0]).join('');
 }
