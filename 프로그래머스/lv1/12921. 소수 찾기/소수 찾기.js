dd3da216d5ff4716d389b65ecafb0a4bb438b106
hjyoo.dev <solteee23@gmail.com>
2023-03-08 11:15:35 +0900

    [level 1] Title: 소수 찾기, Time: 80.33 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -2,10 +2,11 @@ function solution(n) {
     const primes = Array(n+1).fill(true).map((_, i) => i % 2 !== 0);
     
     for (let i = 2; i <= n; ++i) {
+        if (!primes[i]) continue;
         for (let j = 2; j * i <= n; j++) {
             primes[i * j] = false;
         }
     }
-    
+
     return primes.filter((prime) => prime).length;
 }
