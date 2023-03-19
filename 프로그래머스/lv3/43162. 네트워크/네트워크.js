9bc43199eb2f3f1e40c0880f8bb5132d4fbad097
hjyoo.dev <solteee23@gmail.com>
2023-03-19 14:20:14 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,24 @@
+
+
+function solution(n, computers) {
+    let answer = 0;
+    const visited = Array(n).fill(false);
+    
+    function dfs(i) {
+        visited[i] = true;
+        computers[i].forEach((computer, j) => {
+            if (computer && !visited[j]) {
+                dfs(j);
+            }
+        });
+    }
+    
+    computers.forEach((computer, i) => {
+        if (!visited[i]) {
+            dfs(i);
+            answer++;
+        }
+    });
+    
+    return answer;
+}
