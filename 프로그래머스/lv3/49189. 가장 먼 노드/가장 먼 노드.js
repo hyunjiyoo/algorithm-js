4edeaababad70bcde3b6552bd85e8b9c9d173a73
hjyoo.dev <solteee23@gmail.com>
2023-03-19 16:19:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 9961.06 ms, Memory: 49 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,30 @@
+function solution(n, edge) {
+    const visited = Array(n+1).fill(false);
+    const level = Array(n+1).fill(0);
+    const queue = [1];
+    visited[1] = true;
+    
+    while (queue.length) {
+        const target = queue.shift();
+        const lev = level[target] + 1;
+        
+        edge.forEach(([e1, e2]) => {
+            
+            if (e1 === target && !visited[e2]) {
+                visited[e2] = true;
+                queue.push(e2);
+                level[e2] = lev;
+                return;
+            }
+            
+            if (e2 === target && !visited[e1]) {
+                visited[e1] = true;
+                queue.push(e1);
+                level[e1] = lev;
+            }
+        });
+    }
+    
+    const max = Math.max(...level);
+    return level.filter(l => l === max).length;
+}
