e13b4e4b0a1978ea5d8a90740bfac0ac4f68b3ee
hjyoo.dev <solteee23@gmail.com>
2023-03-19 18:41:25 +0900

    [level 3] Title: 숫자 게임, Time: 142.27 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/README.md
+++ 프로그래머스/lv3/12987. 숫자 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987#) 
 
 ### 성능 요약
 
-메모리: 45.7 MB, 시간: 104.08 ms
+메모리: 45.8 MB, 시간: 142.27 ms
 
 ### 구분
 
--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -1,26 +1,20 @@
 function solution(A, B) {
-    let answer = 0;
+    const LEN = B.length;
+    const visited = Array(LEN).fill(false);
+    let start = 0;
     
     A.sort((a,b)=>b-a);
     B.sort((a,b)=>b-a);
-    // console.log(A);
-    // console.log(B);
     
-    let len = B.length;
-    const visited = Array(len).fill(false);
-    const max = B[0];
-    
-    let start = 0;
-    for (let i = 0; i < len; ++i) {
-        for (let j = start; j < len; ++j) {
+    for (let i = 0; i < LEN; ++i) {
+        for (let j = start; j < LEN; ++j) {
             if (!visited[j] && B[j] > A[i]) {
                 visited[j] = true;
                 start = j;
-                ++answer;
                 break;
             }
         }
     }
     
-    return answer;
+    return visited.filter(v => v).length;
 }
