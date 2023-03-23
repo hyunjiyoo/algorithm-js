85492354a558d56acab7d6884e9c1e7cd6ee98cc
hjyoo.dev <solteee23@gmail.com>
2023-03-23 17:42:35 +0900

    [level 2] Title: 숫자의 표현, Time: 0.29 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    if (n === 1) 
+        return 1;
+    
+    let answer = (n % 2 === 1) ? 2 : 1;    
+    for (let i = 2; i <= n / 2 + 1; ++i) {
+        if (n % i === 0 && i % 2 === 1)
+            ++answer;       
+    }
+    
+    return answer;
+}
